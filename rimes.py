f7066c0cbdc2ae478c654f404126e05951c1540a
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-03-23 12:52:04 -0300

    Update rimes.py

--- rimes.py
+++ rimes.py
@@ -5,13 +5,15 @@ import json
 
 def findRimes(string):
 
-    kval_pairs = {'rel_rhy': user_word}                                         # spefify parameters
+    kval_pairs = {'rel_rhy': string}                                            # spefify parameters
     page = requests.get("https://api.datamuse.com/words", params=kval_pairs)    # construct the URL 
-    content = page.json()   # transform the JSON response into a python object
+    content = page.json()                                                       # transform the JSON response into a python object
 
+    words = []                 
     for inside in content:
-        print('A word that rimes with' + user_word + ' is:' , inside['word'])
-
+        words.append(inside['word'])                                            # add each word to the empty list
+        print('A word that rimes with' + user_word + ' is:' , inside['word'])   # print the words on the screen in a nice format
+    return words                                                                # return a list of the words that can be used in other parts of the program
 
 user_word = input('I need words that rime with: ')
 print('---------- Working on it ----------')
